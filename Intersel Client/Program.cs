178e5a46a552d2e3e3c67606d48ff00adf6c58a5
Mr.Bandit <91349463+WCBandit@users.noreply.github.com>
2021-09-28 21:43:01 +0100

    Add files via upload

---
+++ Intersel Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using InterselClient_API;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidLLC_client
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // Auth.GG API
+            OnProgramStart.Initialize("intersel", "79248", "l5eSecq7rYGQUuVDZpOBWYlxFGcRtrSb837", "1.0");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void SetValue(string key, string value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+
+        public static string GetValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ Intersel Client/RevertStub.cs
@@ -0,0 +1,83 @@
+﻿using AsteroidLLC_client.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidLLC_client
+{
+    // If you injected 'index.js' with a stub, you could use this feature to clean the injection.
+    class RevertStub
+    {
+        internal static void RS()
+        {
+            foreach (var i in Discord())
+            {
+                try
+                {
+                    File.WriteAllText(Index(i), "module.exports = require('./core.asar');");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Intersel - Revert Stub Exception");
+                }
+            }
+        }
+
+        static List<string> Discord()
+        {
+            List<string> list = new List<string>();
+            foreach (var i in Directory.GetDirectories(Environment.GetEnvironmentVariable("LocalAppData")))
+            {
+                if (i.Contains("cord"))
+                {
+
+                    list.Add(i);
+                }
+
+            }
+            return list;
+        }
+
+        static string Index(string localappdata)
+        {
+            string path = "";
+            try
+            {
+                foreach (var dir1 in Directory.GetDirectories(localappdata))
+                {
+                    if (dir1.Contains("app-"))
+                    {
+
+                        foreach (var app in Directory.GetDirectories(dir1))
+                        {
+                            if (app.Contains("modules"))
+                            {
+                                foreach (var i in Directory.GetDirectories(app))
+                                {
+                                    if (i.Contains("discord_desktop_core"))
+                                    {
+                                        path = i + @"\discord_desktop_core\index.js";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                path = "Discord is not installed!";
+            }
+            return path;
+        }
+    }
+}
